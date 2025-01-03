47e84b4c128603f5443087061effb2dc8701f194
Shah Nawaz <shuvo1824@hotmail.com>
2025-01-03 10:43:56 +0600

    feat: richtext field added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, :description)
     end
 end
